ce2a2503eed1ec71ea66467ce8bfbd1e821d1b84
thanhhuong050794 <95083739+thanhhuong050794@users.noreply.github.com>
2025-10-22 03:10:10 +0700

    Add files via upload

---
+++ menu-images.js
@@ -0,0 +1,40 @@
+const menuImages = {
+    // 1. Phở bò 
+    "MA01": "https://i.pinimg.com/736x/a4/fb/af/a4fbaf9b0e206c6954fcb105ccc1562b.jpg",
+    
+    // 2. Bánh mì thịt 
+    "MA02": "https://i.pinimg.com/1200x/4f/11/36/4f1136a1af5fbca73d02f6fc6f2b23ac.jpg",
+    
+    // 3. Cơm sườn 
+    "MA03": "https://i.pinimg.com/736x/d3/8d/0a/d38d0aba94e161ac1b338140848558d4.jpg",
+    
+    // 4. Xôi gà 
+    "MA04": "https://i.pinimg.com/1200x/71/34/d5/7134d506475745fc9d14035d30c5030f.jpg",
+    
+    // 5. Mì tôm trứng 
+    "MA05": "https://i.pinimg.com/736x/f7/64/16/f76416c50ca12492903cdc25c9dc524c.jpg",
+    
+    // 6. Nước lọc 
+    "DU01": "https://i.pinimg.com/736x/cd/f8/bd/cdf8bda1bac3232bf6ee912814a94bd6.jpg",
+    
+    // 7. Coca Cola 
+    "DU02": "https://i.pinimg.com/1200x/cf/29/04/cf2904a920ba034d48402aa4c1aabae7.jpg",
+    
+    // 8. Cà phê sữa 
+    "DU03": "https://i.pinimg.com/1200x/ac/e2/ff/ace2fffc49471c96278f15340b5075f1.jpg",
+    
+    // 9. Trà đào 
+    "DU04": "https://i.pinimg.com/736x/13/29/2f/13292f0a2609c317a663c29128414934.jpg",
+    
+    // 10. Nước cam 
+    "DU05": "https://i.pinimg.com/736x/1f/85/8d/1f858d0449f08f22772af491d46639f8.jpg",
+    
+    // 11. Combo 1: Cơm + Nước 
+    "CB01": "https://storage.googleapis.com/onelife-public/blog.onelife.vn/2021/11/cach-lam-suon-nuong-coca-mon-chinh-577223018700.jpg",
+    
+    // 12. Combo 2: Phở + Trà 
+    "CB02": "https://image.vnreview.vn/img.php?src=524288_70849781252284_2203532971212800?wt=a1d3240428f6a1991450584d8381c7c5&rt=c0f8a37223adc252fd9e98b72e302485&width=1080",
+    
+    // 13. Combo 3: Bánh mì + Cafe 
+    "CB03": "https://uploads.nguoidothi.net.vn/content/08fbd200-1ab3-4ac9-90e7-91198a32354c.jpg"
+};
